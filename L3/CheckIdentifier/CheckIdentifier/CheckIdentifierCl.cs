f7b79e79e5a4e0ce88e7d8d45a44e0eb012cd585
Chicony2753 <48485564+Chicony2753@users.noreply.github.com>
2020-05-02 13:02:19 +0300

    lb3_1

---
+++ L3/CheckIdentifier/CheckIdentifier/CheckIdentifierCl.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckIdentifier
+{
+    public class CheckIdentifierCl
+    {
+        static void Main(string[] args)
+        {
+            Check(args);
+        }
+
+        public static int Check(string[] args)
+        {
+            string identifier = args[0];
+            string upLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string lowLetters = "abcdefghijklmnopqrstuvwxyz";
+            string numbers = "0123456789";
+
+            if (args.Length > 1)
+            {
+                Console.WriteLine("You entered more than one identifier!");
+                return 1;
+            }
+
+            if (identifier == "")
+            {
+                Console.WriteLine("Entered empty string!");
+                return 1;
+            }
+
+            if (identifier.Contains(' '))
+            {
+                Console.WriteLine("No.\nIdentifier contains a space!");
+                return 1;
+            }
+
+            for (int i = 0; i < identifier.Length; i++)
+            {
+                if ((!numbers.Contains(identifier[i])) & (!lowLetters.Contains(identifier[i]))
+                    & (!upLetters.Contains(identifier[i])))
+                {
+                    Console.WriteLine("No.\nIdentifier contains non-rule characters!");
+                    return 1;
+                }
+            }
+
+            if (numbers.Contains(identifier[0]))
+            {
+                Console.WriteLine("No.\nCan't start with a number!");
+                return 1;
+            }
+
+            Console.WriteLine("Yes.");
+            return 0;
+        }
+    }
+}
---
+++ L3/CheckIdentifier/CheckIdentifier_Test/CheckIdentifierTest.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Runtime.InteropServices;
+using Xunit;
+using Microsoft.VisualStudio;
+using CheckIdentifier;
+using Xunit.Sdk;
+
+namespace CheckIdentifier_Test
+{
+    public class CheckIdentifierTest
+    {
+        [Fact]
+        // return 1 - error, return 0 - ok
+        public void two_args_return_1()
+        {
+            string[] str = { "sdvsd", "sdfds", "dsfb" };
+            int expected = 1;
+
+            int actual = CheckIdentifierCl.Check(str);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void one_args_return_0()
+        {
+            string[] str = { "sdvsd" };
+            int expected = 0;
+
+            int actual = CheckIdentifierCl.Check(str);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void emptyString_args_return_1()
+        {
+            string[] str = {""};
+            int expected = 1;
+
+            int actual = CheckIdentifierCl.Check(str);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void theseIsASpace_args_return_1()
+        {
+            string[] str = {"aknfj ak"};
+            int expected = 1;
+
+            int actual = CheckIdentifierCl.Check(str);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void unacceptableSymbols_args_return_1()
+        {
+            string[] str = { "ak+324" };
+            int expected = 1;
+
+            int actual = CheckIdentifierCl.Check(str);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void firstDigitCharacter_args_return_1()
+        {
+            string[] str = { "3dgs" };
+            int expected = 1;
+
+            int actual = CheckIdentifierCl.Check(str);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
